d00004092d1512b696b70ec85ef149f520b35eac
Y1nzhenghao <17636275135@163.com>
2026-03-11 23:13:53 +0000

    Set up initial Django models and database migration

--- bookrealm/models.py
+++ bookrealm/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=13, unique=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    numPages = models.IntegerField()
+    cover_image = models.ImageField(upload_to='covers/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    pass
+
+
+class Follow(models.Model):
+    pass
+
+
+class Wishlist(models.Model):
+    pass
