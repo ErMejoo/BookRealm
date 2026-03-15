4f8e9248885f65e994372c4a1e31f5b3333cfbb5
Arthur Lytvynov <3017810l@student.gla.ac.uk>
2026-03-15 23:25:26 +0000

    Add wishlist functionality with add/remove actions and confirmation messages

--- bookrealm/views.py
+++ bookrealm/views.py
@@ -5,8 +5,9 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Follow
+from .models import Follow, Wishlist, Book
 from datetime import datetime
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -17,14 +18,61 @@ def browse(request):
     response = render(request, 'bookrealm/browse.html')
     return response
 
+def chosen_author(request, user_id):
+    try:
+        author = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        author = None
+
+    return render(request, 'bookrealm/chosenAuthor.html', {'author': author})
+
+def chosen_book(request, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        book = None
+
+    return render(request, 'bookrealm/chosenBook.html', {'book': book})
+
 @login_required
 def follow_user(request, user_id):
     user_to_follow = User.objects.get(id=user_id)
     Follow.objects.get_or_create(follower=request.user, following=user_to_follow)
-    return redirect(reverse('BookRealm:browse'))
+    return redirect(reverse('BookRealm:chosen_author', args=[user_id]))
 
 @login_required
 def unfollow_user(request, user_id):
     user_to_unfollow = User.objects.get(id=user_id)
     Follow.objects.filter(follower=request.user, following=user_to_unfollow).delete()
-    return redirect(reverse('BookRealm:browse'))
+    return redirect(reverse('BookRealm:chosen_author', args=[user_id]))
+
+@login_required
+def add_to_wishlist(request, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+        Wishlist.objects.get_or_create(user=request.user, book=book)
+        messages.success(request, "Book added to the wishlist")
+    except Book.DoesNotExist:
+        pass
+    return redirect(reverse('BookRealm:chosen_book', args=[book_id]))
+
+@login_required
+def remove_from_wishlist(request, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+        Wishlist.objects.filter(user=request.user, book=book).delete()
+        messages.success(request, "Book removed from the wishlist")
+    except Book.DoesNotExist:
+        pass
+    next_page = request.GET.get('next')
+    if next_page == "book":
+        return redirect(reverse("BookRealm:chosen_book", args=[book_id]))
+    return redirect(reverse('BookRealm:view_wishlist'))
+
+@login_required
+def view_wishlist(request):
+    wishlist_items = Wishlist.objects.filter(user=request.user).select_related('book')
+    return render(request, 'bookrealm/wishlist.html', {'wishlist_items': wishlist_items})
+
+
+    
