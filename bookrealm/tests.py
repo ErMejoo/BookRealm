4e6c256313a8a22b8160e424d744ba2388cffd7d
Y1nzhenghao <17636275135@163.com>
2026-03-25 14:39:40 +0000

    Add unit tests for models, views, review submission and rating logic

--- bookrealm/tests.py
+++ bookrealm/tests.py
@@ -1,3 +1,94 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Book, Review
+from django.urls import reverse
 
-# Create your tests here.
+class BookModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username='testuser')
+        self.book = Book.objects.create(
+            isbn='1234567890123',
+            title='Test Book',
+            description='Test description',
+            numPages=100,
+            created_by=self.user
+        )
+
+    def test_book_creation(self):
+        self.assertEqual(self.book.title, 'Test Book')
+        self.assertEqual(self.book.numPages, 100)
+
+    def test_review_creation(self):
+        review = Review.objects.create(
+            user=self.user,
+            book=self.book,
+            rating=5,
+            comment_text="Great book!"
+        )
+
+        self.assertEqual(review.book, self.book)
+        self.assertEqual(review.user, self.user)
+        self.assertEqual(review.rating, 5)
+
+class ViewTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+    def test_home_page(self):
+        response = self.client.get(reverse('BookRealm:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        login = self.client.login(username='testuser', password='12345')
+        self.assertTrue(login)
+
+class AddReviewViewTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+        self.book = Book.objects.create(
+            isbn='1234567890123',
+            title='Test Book',
+            description='Test',
+            numPages=100,
+            created_by=self.user
+        )
+
+    def test_add_review(self):
+        self.client.login(username='testuser', password='12345')
+
+        response = self.client.post(
+            reverse('BookRealm:add_review', args=[self.book.id]),
+            {
+                'rating': 5,
+                'comment_text': 'Nice book'
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)  # redirect
+        self.assertEqual(Review.objects.count(), 1)
+
+class RatingLogicTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username='testuser')
+
+        self.book = Book.objects.create(
+            isbn='1234567890123',
+            title='Test Book',
+            description='Test',
+            numPages=100,
+            created_by=self.user
+        )
+
+    def test_average_rating(self):
+        Review.objects.create(user=self.user, book=self.book, rating=4, comment_text="Good")
+        Review.objects.create(user=self.user, book=self.book, rating=2, comment_text="Bad")
+
+        reviews = Review.objects.filter(book=self.book)
+        avg = sum(r.rating for r in reviews) / reviews.count()
+
+        self.assertEqual(avg, 3)
